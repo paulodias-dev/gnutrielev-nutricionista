dc06163476e40aa2a2c084f78ea9440183ae189d
Paulo Roberto Dias <paulo.roberto@startcorp.com.br>
2026-07-01 09:30:01 -0300

    Completa metatags Open Graph e Twitter

--- src/components/seo/SEO.tsx
+++ src/components/seo/SEO.tsx
@@ -22,32 +22,36 @@ export const SEO: React.FC<SEOProps> = ({ title, description, keywords, canonica
   const pageDescription = description?.trim() || seoConfig.description;
   const pageKeywords = keywords?.length ? keywords : seoConfig.keywords;
   const canonicalUrl = buildCanonicalUrl(canonicalPath);
+  const ogImage = seoConfig.openGraph.images[0];
 
   return (
     <Helmet>
       <title>{pageTitle}</title>
       <meta name="description" content={pageDescription} />
       <meta name="keywords" content={pageKeywords.join(', ')} />
-      <meta name="robots" content={noIndex ? 'noindex, nofollow' : 'index, follow'} />
+      <meta name="robots" content={noIndex ? 'noindex, nofollow' : 'index, follow, max-image-preview:large'} />
       <link rel="canonical" href={canonicalUrl} />
 
-      <meta property="og:title" content={pageTitle} />
-      <meta property="og:description" content={pageDescription} />
+      <meta property="og:title" content={seoConfig.openGraph.title || pageTitle} />
+      <meta property="og:description" content={seoConfig.openGraph.description || pageDescription} />
       <meta property="og:url" content={canonicalUrl} />
       <meta property="og:site_name" content={seoConfig.openGraph.siteName} />
       <meta property="og:type" content={seoConfig.openGraph.type} />
       <meta property="og:locale" content={seoConfig.openGraph.locale} />
-      <meta property="og:image" content={seoConfig.openGraph.images[0].url} />
-      <meta property="og:image:width" content={`${seoConfig.openGraph.images[0].width}`} />
-      <meta property="og:image:height" content={`${seoConfig.openGraph.images[0].height}`} />
-      <meta property="og:image:alt" content={seoConfig.openGraph.images[0].alt} />
+      <meta property="og:image" content={ogImage.url} />
+      <meta property="og:image:secure_url" content={ogImage.secureUrl} />
+      <meta property="og:image:type" content={ogImage.type} />
+      <meta property="og:image:width" content={`${ogImage.width}`} />
+      <meta property="og:image:height" content={`${ogImage.height}`} />
+      <meta property="og:image:alt" content={ogImage.alt} />
 
       <meta name="twitter:card" content={seoConfig.twitter.card} />
       <meta name="twitter:site" content={seoConfig.twitter.site} />
       <meta name="twitter:creator" content={seoConfig.twitter.creator} />
-      <meta name="twitter:title" content={pageTitle} />
-      <meta name="twitter:description" content={pageDescription} />
-      <meta name="twitter:image" content={seoConfig.openGraph.images[0].url} />
+      <meta name="twitter:title" content={seoConfig.openGraph.title || pageTitle} />
+      <meta name="twitter:description" content={seoConfig.openGraph.description || pageDescription} />
+      <meta name="twitter:image" content={ogImage.url} />
+      <meta name="twitter:image:alt" content={ogImage.alt} />
     </Helmet>
   );
 };
